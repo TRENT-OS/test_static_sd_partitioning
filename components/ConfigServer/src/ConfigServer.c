f2dc75905bede2dbbfe6153a638af8ccd7675d9b
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-26 14:35:33 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/ConfigServer/src/ConfigServer.c
+++ components/ConfigServer/src/ConfigServer.c
@@ -1,9 +1,13 @@
 /*
-   *  ConfigServer component that can be initialized either with a memory or a filesystem
-   *  backend.
-   *
-   *  Copyright (C) 2019, Hensoldt Cyber GmbH
-*/
+ * ConfigServer component that can be initialized either with a memory or a filesystem
+ * backend.
+ *
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
+ */
 
 
 #include <stdio.h>
